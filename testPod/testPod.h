6356fcc13aa6187507414a9f237c8ed037bf82da
Roy <roy.dourng@pathmazing.com>
2019-03-04 16:19:46 +0700

    Initial Commit

---
+++ testPod/testPod.h
@@ -0,0 +1,19 @@
+//
+//  testPod.h
+//  testPod
+//
+//  Created by roy on 3/4/19.
+//  Copyright © 2019 Pathmazing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testPod.
+FOUNDATION_EXPORT double testPodVersionNumber;
+
+//! Project version string for testPod.
+FOUNDATION_EXPORT const unsigned char testPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testPod/PublicHeader.h>
+
+
